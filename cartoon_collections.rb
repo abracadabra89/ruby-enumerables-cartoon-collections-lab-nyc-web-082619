35c771ea9f3bde526943726528394bd7a9ee8b34
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-15 21:19:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(array)# code an argument here
 end
 
 def summon_captain_planet(array)# code an argument here
-  array.collect 
+  array.map(&:upcase)
 end
 
 def long_planeteer_calls# code an argument here
