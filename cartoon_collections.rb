f5667e648d9ba1df79c62a977d417ca95ce707e6
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-15 21:36:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,8 +20,12 @@ end
 
 
 def find_the_cheese(array)
+  i = 0
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include?(cheese_types)
-  array[0]
+  while i < cheese_types.length
+    return cheese_types[i] if array.include?(cheese_types[i])
+    i += 1
+  end
+end
 end
 
