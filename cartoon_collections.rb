f68f96628cc7f7555c53e14d6bd0c63391d9a213
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-15 21:36:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,12 +12,12 @@ def summon_captain_planet(planeteer_calls)
 end
 
 
-def long_planeteer_calls(array)
- array.select {|a| a >= 4} 
-    puts true
-  else
-    puts false 
+def long_planeteer_calls(calls)
+ calls.any? do |call|
+    call.length > 4
+  end
 end
+
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
