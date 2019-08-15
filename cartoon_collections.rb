714a21a170459a077e273b44c2b091fa5814ca39
anya litvinova <anya.s.litvinova@gmail.com>
2019-08-15 21:33:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,9 @@ def long_planeteer_calls(array)
     puts false 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?(cheese_types)
+  array(0)
 end
+
